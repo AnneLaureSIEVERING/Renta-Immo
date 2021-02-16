17acd42b706684f0ef17881f0efd18d024a56150
AnneLaureSIEVERING <anne-laure@oxynum.fr>
2021-02-16 16:23:33 +0100

    integration JS and HTML done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let price = document.querySelector('#price');
 let notary = document.querySelector('#notary');
 let agency = document.querySelector('#agency');
@@ -10,25 +12,7 @@ let classResults = document.querySelector('.classResults');
 
 let nRent;
 let priceTotal;
-
-
-
-// Event sur la sortie de l'input concernant le loyer pour un affichage dynamique de la somme sur 1 an
-rent.addEventListener('blur', (e) => {
-    e.preventDefault;
-    nRent = rentByYear(rent);
-    displayRentByYear(nRent);
-});
-
-
-// Event au clic sur le boutton "calculer"
-button.addEventListener('click', (e)=> {
-    e.preventDefault;
-    priceTotal = totaPriceImmo(price, notary, agency, roadworks);
-    
-    displayProfitability(priceTotal, nRent);
-    displayAmountProfitability(priceTotal, nRent, 10)
-});
+let pxFees;
 
 // calcul automatique du montant annuel que vont représenter les loyers
 function rentByYear(rt) {
@@ -50,6 +34,27 @@ function interestCompose(priceTotal, rt, nYear) {
     return priceTotal * Math.pow((1 + rt / priceTotal), nYear) - priceTotal;
 }
 
+// Fonction pour calculer les frais de notaire automatiquement
+function notaryFees(pxImmo) {
+    let priceImmo = pxImmo.value;
+
+    switch(true) {
+        case (priceImmo <= 6500):
+            return (priceImmo * (3.870/100)) * 1.20 + 800 + 400 + (priceImmo * 0.058) + (priceImmo * 0.0010);
+
+        case (priceImmo >= 6501 && priceImmo <= 17000):
+            return (priceImmo * (1.596/100) + 147.81) * 1.20 + 800 + 400 + (priceImmo * 0.058) + (priceImmo * 0.0010);
+
+        case (priceImmo >= 17001 && priceImmo <= 60000):
+            return (priceImmo * (1.064/100) + 238.25) * 1.20 + 800 + 400 + (priceImmo * 0.058) + (priceImmo * 0.0010);
+        
+        case(priceImmo > 60000):
+            return (priceImmo * (0.799/100) + 397.25) * 1.20 + 800 + 400 + (priceImmo * 0.058) + (priceImmo * 0.0010);
+
+        default:
+            console.log('erreur dans le switch');
+    }
+}
 
 // Liste des fonctions pour l'intégration dans le DOM des résultats
 function displayProfitability(pxTotal, nRtx) {
@@ -72,6 +77,33 @@ function displayRentByYear(valueRentByYear) {
     classRent.appendChild(valueRent);
 }
 
-// TODO : Fonction pour calculer les frais de notaire automatiquement
+function displayNotaryFees(valueFees) {
+    notary.setAttribute("value", Math.round(valueFees));
+}
+
+// Event sur la sortie de l'input concernant le loyer pour un affichage dynamique de la somme sur 1 an
+rent.addEventListener('blur', (e) => {
+    e.preventDefault;
+    nRent = rentByYear(rent);
+    displayRentByYear(nRent);
+});
+
+// Event qui va calculer automatiquement les frais de notaire selon le prix d'achat du bien
+price.addEventListener('blur', (e) => {
+    e.preventDefault;
+    pxFees = notaryFees(price)
+    displayNotaryFees(pxFees);
+});
+
+
+// Event au clic sur le boutton "calculer"
+button.addEventListener('click', (e)=> {
+    e.preventDefault;
+    priceTotal = totaPriceImmo(price, notary, agency, roadworks);
+    
+    displayProfitability(priceTotal, nRent);
+    displayAmountProfitability(priceTotal, nRent, 10)
+});
+
 
 
