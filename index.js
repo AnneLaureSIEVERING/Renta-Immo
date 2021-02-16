c477115eef062398296ce45700c1f81bd3d13fbc
AnneLaureSIEVERING <anne-laure@oxynum.fr>
2021-02-16 11:39:13 +0100

    refacto code

--- index.js
+++ index.js
@@ -8,60 +8,68 @@ let button = document.querySelector('#button');
 let classRent = document.querySelector('.classRent');
 let classResults = document.querySelector('.classResults');
 
-let rentByYear;
+let nRent;
 let priceTotal;
-let resultProfitability;
-let amountInterest;
-let amount;
 
-// calcul automatique du montant annuel que vont représenter les loyers
-rent.addEventListener('blur', (rentValue) => {
-    rentValue = Number(rent.value);
-    rentByYear = rentValue * 12;
-    console.log(rentByYear);
-    let valueRent = document.createElement('p');
-    valueRent.textContent = '(équivaut à ' + rentByYear + ' € par an)';
-    classRent.appendChild(valueRent);
-})
 
-// Fonctions à déclencher au clic sur le boutton "calculer"
-button.addEventListener('click', (e)=> {
+
+// Event sur la sortie de l'input concernant le loyer pour un affichage dynamique de la somme sur 1 an
+rent.addEventListener('blur', (e) => {
     e.preventDefault;
-    sum();
-    profitability();
-    interestCompose();
-    integrationHtml();
+    nRent = rentByYear(rent);
+    displayRentByYear(nRent);
 });
 
-// Fonction somme totale des datas du formulaire
-function sum() {
-    priceTotal = Number(price.value) + Number(notary.value) + Number(agency.value) + Number(roadworks.value) ;
-    console.log(priceTotal);
+
+// Event au clic sur le boutton "calculer"
+button.addEventListener('click', (e)=> {
+    e.preventDefault;
+    priceTotal = totaPriceImmo(price, notary, agency, roadworks);
+    
+    displayProfitability(priceTotal, nRent);
+    displayAmountProfitability(priceTotal, nRent, 10)
+});
+
+// calcul automatique du montant annuel que vont représenter les loyers
+function rentByYear(rt) {
+    return Number(rt.value) * 12;
 }
 
-// Fonction qui permet de calculer le taux de rentabilité de l'investissement
-function profitability() {
-    let result = rentByYear / priceTotal;
-    resultProfitability = Math.round(result * 100);
-    console.log(resultProfitability);
+// Fonction somme totale des datas du formulaire (hors loyer estimé)
+function totaPriceImmo(px, ntx, agx, rdwk) {
+    return Number(px.value) + Number(ntx.value) + Number(agx.value) + Number(rdwk.value);
 }
 
-// Fonction qui permet de calculer la rentabilité d'un investissement avec interet compose (sur 10 ans)
-function interestCompose() {
-    amountInterest = Math.round(priceTotal * Math.pow((1.10), 10));
-    console.log(amountInterest);
-    amount = amountInterest - priceTotal;
+// Fonction qui permet de calculer le taux de rentabilité de l'investissement (en pourcentage)
+function profitability(pxTotal, rt) {
+    return Math.round(rt / pxTotal * 100);
 }
 
-// Fonction qui va permettre d'intégrer les datas dans le HTML
-function integrationHtml() {
-    let integrationValueResult = document.createElement('p');
-    integrationValueResult.textContent = 'Rentabilité brute : ' + resultProfitability + ' %';
-    classResults.appendChild(integrationValueResult);
+// Fonction qui permet de calculer le montant de la rentabilité d'un investissement avec interet compose
+function interestCompose(priceTotal, rt, nYear) {
+    return priceTotal * Math.pow((1 + rt / priceTotal), nYear) - priceTotal;
+}
 
-    let integrationValueAmount = document.createElement('p');
-    integrationValueAmount.textContent = 'Rentabilité sur 10 ans : ' + amount + ' €';
-    classResults.appendChild(integrationValueAmount);
+
+// Liste des fonctions pour l'intégration dans le DOM des résultats
+function displayProfitability(pxTotal, nRtx) {
+    let profiTx = profitability(pxTotal, nRtx);
+    let displayValueResult = document.createElement('p');
+    displayValueResult.textContent = 'Rentabilité brute : ' + profiTx + ' %';
+    classResults.appendChild(displayValueResult);
+}
+
+function displayAmountProfitability(pxTotal, nRent, nYear) {
+    let valueAmount = interestCompose(pxTotal, nRent, nYear);
+    let displayValueAmount = document.createElement('p');
+    displayValueAmount.textContent = 'Rentabilité sur 10 ans : ' + Math.round(valueAmount) + ' €';
+    classResults.appendChild(displayValueAmount);
+}
+
+function displayRentByYear(valueRentByYear) {
+    let valueRent = document.createElement('p');
+    valueRent.textContent = '(équivaut à ' + valueRentByYear + ' € par an)';
+    classRent.appendChild(valueRent);
 }
 
 // TODO : Fonction pour calculer les frais de notaire automatiquement
